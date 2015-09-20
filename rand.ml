125dbb0e4ca7ba9ef3f2e3b1d2a4301559297ea5
Anton Yabchinskiy <arn@bestmx.ru>
2015-09-21 01:26:40 +0300

    rand: don't return DEL from rand char function

--- rand.ml
+++ rand.ml
@@ -4,7 +4,7 @@ let xi () = Random.float 1.0
 
 let int ?(min=0) ~max () = min + Random.int (max - min + 1)
 
-let char () = Char.chr (int ~min:32 ~max:127 ())
+let char () = Char.chr (int ~min:32 ~max:126 ())
 
 (* TODO: check distr probs to sum to 1.0 *)
 let choice distr =
