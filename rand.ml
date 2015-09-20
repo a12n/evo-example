7540d62d86c77fbc1dfc09faa182a9491a7de9cd
Anton Yabchinskiy <arn@bestmx.ru>
2015-09-21 01:08:54 +0300

    Import Rand module

---
+++ rand.ml
@@ -0,0 +1,23 @@
+open Batteries
+
+let xi () = Random.float 1.0
+
+let int ?(min=0) ~max () = min + Random.int (max - min + 1)
+
+let char () = Char.chr (int ~min:32 ~max:127 ())
+
+(* TODO: check distr probs to sum to 1.0 *)
+let choice distr =
+  let p = ref (xi ()) in
+  Array.find
+    (fun (p_i, _a_i) ->
+       if !p < p_i then
+         true
+       else
+         ( p := !p -. p_i;
+           false ))
+    distr |> snd
+
+let bool ?(p=0.5) () = xi () < p
+
+let seed = Random.self_init
---
+++ rand.mli
@@ -0,0 +1,18 @@
+(** [true] with probability [p] (0.5 by default). *)
+val bool : ?p:float -> unit -> bool
+
+(** ASCII character with uniform distribution. *)
+val char : unit -> char
+
+(** Choose a single [a] according to the distribution. *)
+val choice : (float * 'a) array -> 'a
+
+(** Integer from [min] (0 by default) to [max] inclusive with uniform
+    distribution. *)
+val int : ?min:int -> max:int -> unit -> int
+
+(** Initialize with more or less random seed. *)
+val seed : unit -> unit
+
+(** Canonical uniform random variable. *)
+val xi : unit -> float
