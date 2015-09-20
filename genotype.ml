5cc544822f0d296ebd945998b302d639b531d7f9
Anton Yabchinskiy <arn@bestmx.ru>
2015-09-21 02:04:08 +0300

    genotype: mutation should (possibly) occur for each gene

--- genotype.ml
+++ genotype.ml
@@ -2,17 +2,19 @@ open Batteries
 
 type t = string
 
+let crossover a _b =
+  (* TODO *)
+  a
+
 let fitness ~ideal genes =
   (-1) * String.edit_distance ideal genes
 
 let mutated ?(p=0.05) genes =
-  if Rand.bool ~p () then
-    ( let max = String.length genes - 1 in
-      let k = Rand.int ~max () in
-      let copy = String.copy genes in
-      copy.[k] <- Rand.char ();
-      copy )
-  else
-    genes
+  String.(init (length genes)
+            (fun i ->
+               if Rand.bool ~p () then
+                 Rand.char ()
+               else
+                 genes.[i]))
 
 let random len = String.init len (fun _i -> Gene.random ())
